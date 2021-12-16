1c5ce4703951f781fc2f57e1ae40cdeaf0d6c6f4
manin27 <55440870+manin27@users.noreply.github.com>
2021-12-16 18:11:58 +0300

    [2]корреляция
    Лаба

---
+++ Манин К.А/2.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("C:/Users/manin/OneDrive/Documents/GitHub/2021-intelli-analyze/Манин К.А/character-deaths.csv")
+
+print("Манин Кирилл ИС-27")
+print("Лабораторная работа №1\nОценка взаимосвязей, корреляция")
+print("Задание а")
+print("Проанализировать и выдать статистические данные связи между:\n"
+      "Домом и разницей в главах")
+
+data = data[['Allegiances', 'Death Chapter', 'Book Intro Chapter']]
+data = data.drop(np.where(np.isnan(data['Death Chapter']))[0]).fillna(0)
+data['Chapter diff'] = data['Death Chapter'] - data['Book Intro Chapter']
+data = data.drop(['Death Chapter', 'Book Intro Chapter'], axis=1).groupby('Allegiances').mean()
+data = (data - data.min()) / (data.max() - data.min())
+
+print("Связи между: домом и разницей в главах " + str(data)) 
+
